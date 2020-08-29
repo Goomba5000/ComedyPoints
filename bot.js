78b4f5aa311d449bde33647aecf507f35f0f1da2
Goomba5000 <b0bh3nry@gmail.com>
2020-08-29 19:29:38 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,4 +8,4 @@ client.on("message", msg => {
     msg.reply("Pong!")
   }
 })
-client.login("NzQ5Mzg2MDYxMTY3MTk4MzM4.X0rOJw.4tbT5e-wv4c1uJkmoIuk33LDAto");
+client.login("NzQ5Mzg2MDYxMTY3MTk4MzM4.X0rOJw.G-4K9t_jh5balbqx_t1itGxRtns");
