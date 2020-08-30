d81fdb210c02240c914a56b77e65915b188d1e5c
Goomba5000 <b0bh3nry@gmail.com>
2020-08-29 20:20:28 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,18 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
+    console.log(`Logged in as ${client.user.tag}!`)
 })
-client.on("message", msg => {
-  if (msg.content === "!comedy") {
-    msg.reply(Math.floor(Math.random() * 1000) + " comedy points.");
-  }
+client.on("message", message => {
+    if (message.content === "!comedy") {
+        message.reply(Math.floor(Math.random() * 1000) + " comedy points.");
+    }
+
+    const filter = (reaction) => reaction.emoji.name === '\:comedypoints:'
+    message.awaitReactions(filter, { time: 5000})
+        .then(collected => {
+            if (collected.first().emoji.name == '\:comedypoints:')
+                message.reply(Math.floor(Math.random() * 1000) + " comedy points.");
+        });
 })
 client.login(process.env.BOT_TOKEN);
