cf9ec81f7d82d4d1e1505d03a82afff948b777b8
Goomba5000 <b0bh3nry@gmail.com>
2020-08-30 16:31:40 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,13 +5,13 @@ client.on("ready", () => {
 })
 client.on("message", message => {
     const react = client.emojis.find(emoji => emoji.name === "testimage");
-    const filter = (reaction) => reaction.emoji.name === react
+    const filter = (reaction) => reaction.emoji.tag === "749496316685582366"
     message.awaitReactions(filter, { max: 1 })
         .then(collected => {
             if (collected.first() != null)
             {
                 console.log(collected.first().emoji.name);
-                if (collected.first().emoji.name === react)
+                if (collected.first().emoji.tag === "749496316685582366")
                 {
                     message.reply(Math.floor(Math.random() * 70) + " comedy points.");
                 }
