5874c4973f918dcabd9be9ef65019b083d0a1ca2
Goomba5000 <b0bh3nry@gmail.com>
2020-08-30 01:12:22 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,13 +4,13 @@ client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })
 client.on("message", message => {
-    const filter = (reaction) => reaction.emoji.name === ':comedypoints:'
+    const filter = (reaction) => reaction.emoji.name === '<:testimage:749496316685582366>'
     message.awaitReactions(filter, { max: 1 })
         .then(collected => {
             if (collected.first() != null)
             {
                 console.log(collected.first().emoji.name);
-                if (collected.first().emoji.name === ':comedypoints:')
+                if (collected.first().emoji.name === '<:testimage:749496316685582366>')
                 {
                     message.reply(Math.floor(Math.random() * 70) + " comedy points.");
                 }
